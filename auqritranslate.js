e460627f991ddcce067e6167a6c1e900a604234a
auqri <75044423+auqri@users.noreply.github.com>
2020-11-26 03:47:48 +0700

    Create auqritranslate.js
    Google Translate Country Code.

---
+++ auqritranslate.js
@@ -0,0 +1,7 @@
+function googleTranslateElementInit() {new google.translate.TranslateElement({pageLanguage: &#39;id&#39;, includedLanguages: &#39;id,en,vi,zh-CN,th,ru,ms,ko,ja,it,hi,fr,es,de,ar,tr&#39;,
+layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, &#39;google_translate_element&#39;);} 
+  $(&quot;.hover&quot;).mouseleave(
+    function () {
+      $(this).removeClass(&quot;hover&quot;);
+    }
+  );
